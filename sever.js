d24c3086fa4320ba0cb9c2ec507ef3c2cdb5f94a
Ch4Rmn <linhtut123451@gmail.com>
2023-11-25 22:08:35 +0630

    404 unsupported routes

--- sever.js
+++ sever.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const createError = require("http-errors");
 const port = process.env.PORT || 5000;
 const db = require("./db/lib");
 const morgan = require("morgan");
@@ -13,8 +14,26 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) =>
   res.json({
-    data: "hello",
+    data: "hello i m Home!",
   })
 );
+
+//404 start ( should have in the end)
+// middleware
+app.use((req, res, next) => {
+  next(createError.NotFound("unsupported Route"));
+});
+
+// error
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+//404 end
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
