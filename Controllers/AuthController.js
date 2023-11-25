769beefccce646e5f5cf47cebbca845f5c12bacf
Ch4Rmn <linhtut123451@gmail.com>
2023-11-26 01:04:34 +0630

    cant register same email,need to fill email or pass,bcrypt.Hash Pass,jwt.sign,done Register

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,10 +1,45 @@
-const User = require("../Models/AuthSchema");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+const User = require("../Models/AuthSchema.js");
 
 const RegisterUser = async (req, res, next) => {
-  console.log(req.body);
-    res.send("register");
+  // Create
+  const { email, password } = req.body;
+  if (!email || !password) throw createError.BadRequest();
+
+  const existingUser = await User.findOne({ email: email });
+
+  try {
+    if (!existingUser) {
+      const hashPass = await bcrypt.hash(password, 10);
+
+      //   const User cant same with model User
+      const user = await User.create({
+        email: email,
+        password: hashPass,
+      });
+
+      //   jwt
+      const token = jwt.sign(
+        { email: user.email, id: user._id },
+        process.env.ACCESS_TOKEN,
+        {
+          expiresIn: "1h",
+        }
+      );
+
+      res.status(201).json({
+        User: user,
+        token: token,
+        message: "User created (Complete)",
+      });
+    } else {
+      throw createError.Conflict(`${email} has been already Register`);
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 const loginUser = async (req, res, next) => {};
